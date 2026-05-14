e583f64a1f17fcd920aef1ad2a0d0491aef0e92d
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-05-14 19:58:21 +0300

    setting up react router

--- FindIt/src/App.jsx
+++ FindIt/src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
