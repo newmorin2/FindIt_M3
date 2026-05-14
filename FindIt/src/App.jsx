aa63834b6e47d4cf1fed528df8041121ac5d614e
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-05-14 21:34:12 +0300

    wrapping dashboard

--- FindIt/src/App.jsx
+++ FindIt/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import ProtectedRoute from './routes/ProtectedRoute'
 import './App.css'
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
     </>
