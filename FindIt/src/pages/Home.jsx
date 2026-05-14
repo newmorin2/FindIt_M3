b88fca2da5f907b7e150a879c47fd8875f7e181b
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-05-14 19:52:31 +0300

    creating some folders

---
+++ FindIt/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl font-bold">
+        FindIt
+      </h1>
+
+      <p className="mt-4 text-lg">
+        Lost and Found made simple.
+      </p>
+    </div>
+  )
+}
+
+export default Home
