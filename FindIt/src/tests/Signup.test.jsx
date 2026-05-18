5ff3544cfc1db5636157b7d88ea0435ea1459031
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-05-18 10:05:25 +0300

    put mock

--- FindIt/src/tests/Signup.test.jsx
+++ FindIt/src/tests/Signup.test.jsx
@@ -1,7 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-
+import Login from "../pages/Login";
+import { vi } from "vitest";
 import Signup from "../pages/Signup";
+
+vi.mock("firebase/auth", () => ({
+  getAuth: vi.fn(() => ({})),
+  signInWithEmailAndPassword: vi.fn(),
+  GoogleAuthProvider: vi.fn(),
+  signInWithPopup: vi.fn()
+}));
 
 test("renders signup page", () => {
   render(
